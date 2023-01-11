a57107cc5a24652ded4b3210f6c8b0dbec531259
devasmi <impokhrelasmita5@gmail.com>
2023-01-11 18:09:11 -0500

    added icon and changed data to use 1 pm EST

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var currentWeatherContainer = $("#current-weather");
 var futureWeatherContainer = $("#future-weather");
 var savedCitiesEl = $("#saved-cities");
 var date = dayjs().format("MM/DD/YYYY");
+var baseImageUrl = "http://openweathermap.org/img/wn/";
 
-function createCard(title, temp, wind, humidity, cardClass) {
+function createCard(title, temp, wind, humidity, iconName, cardClass) {
   // create div with class card
   // <div class="card">
   var cardEl = document.createElement("div");
@@ -26,6 +27,9 @@ function createCard(title, temp, wind, humidity, cardClass) {
   // add title inside the card body
   cardBodyEl.appendChild(cardTitle);
 
+  var imgEl = document.createElement("img");
+  imgEl.src = baseImageUrl + iconName + "@2x.png";
+
   var tempEl = document.createElement("p");
   tempEl.textContent = "Temp: " + temp + " °F";
 
@@ -35,6 +39,7 @@ function createCard(title, temp, wind, humidity, cardClass) {
   var humidityEl = document.createElement("p");
   humidityEl.textContent = "Humidity: " + humidity + " %";
 
+  cardBodyEl.append(imgEl);
   cardBodyEl.append(tempEl);
   cardBodyEl.append(humidityEl);
   cardBodyEl.append(windEl);
@@ -59,7 +64,7 @@ function get5DayForecast(cityName) {
       $(futureWeatherContainer).empty();
       // get 1 single data point for each day so total 5 data point
       // using 12:00:00 as the time for each day, going up by 8 step for each day
-      for (var i = 4; i < data.list.length; i = i + 8) {
+      for (var i = 6; i < data.list.length; i = i + 8) {
         var weatherData = data.list[i];
         var weatherDate = dayjs(weatherData.dt_txt).format("MM/DD/YYYY");
         var weatherCard = createCard(
@@ -67,6 +72,7 @@ function get5DayForecast(cityName) {
           weatherData.main.temp,
           weatherData.wind.speed,
           weatherData.main.humidity,
+          weatherData.weather[0].icon,
           "small-card"
         );
         futureWeatherContainer.append(weatherCard);
@@ -94,6 +100,7 @@ function getTodaysForecast(cityName) {
         data.main.temp,
         data.wind.speed,
         data.main.humidity,
+        data.weather[0].icon,
         ""
       );
       currentWeatherContainer.append(currentWeatherCardEl);
